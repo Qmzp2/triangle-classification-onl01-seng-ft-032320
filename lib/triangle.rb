1fa6d38e244eb7575e5f7451c5354969889ef2a1
Kevin Mathias <komathias1@gmail.com>
2020-04-10 03:34:36 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,48 +1,39 @@
 class Triangle
- attr_accessor :length1, :length2, :length3
- 
- def initialize(length1, length2, length3)
-   @length3 = length3
-   @length2 = length2
-   @length1 = length1
-   @all_lengths = []
-   @all_lengths << @length1 
-   @all_lengths << @length2
-   @all_lengths << @length3
- end
- 
- def kind
-   if @all_lengths.any?{|length| length == 0}
-     begin
-     raise TriangleError
-     rescue TriangleError => error
-     puts error.message
-   end
-   elsif (@length1+@length2 <= @length3) || (@length1+@length3 <= @length2) || (@length2+@length3 <= @length1)
-    begin
-     raise TriangleError
-     rescue TriangleError => error
-     puts error.message
-   end
- else
-      if (@length1 == @length2) && (@length2 == @length3)
-        :equilateral
-      elsif (@length1 == @length2) || (@length2 == @length3) || (@length1 == @length3)
-        :isosceles
-      elsif (@length1 != @length2) && (@length2 != @length3) && (@length1 != @length3)
-        :scalene
-      end
-    end
+  attr_accessor :x, :y, :z
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+  end
 
+  def kind
+    if kosher_triangle == true && kosher_triangle2 == true 
+      puts true 
+    else 
+      raise TriangleError 
+    end 
+    
+    if x == y && y == z && x == z
+      :equilateral
+    elsif x != y && y != z && z != x
+      :scalene 
+    else 
+      :isosceles
+    end 
+  end
+  
+  def kosher_triangle 
+    x.positive? && y.positive? && z.positive?
+  end 
+  
+  def kosher_triangle2 
+    x + y > z && x + z > y && y + z > x
+  end 
+  
+  
 end
 
 class TriangleError < StandardError
-  def message
-    "idk what I'm doing"
-  end
-end
-
-
-
+  
 end
 
