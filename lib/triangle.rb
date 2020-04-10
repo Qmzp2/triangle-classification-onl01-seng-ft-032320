aeab3a686cb8cfa614d082bf0f1429ae40808649
Kevin Mathias <komathias1@gmail.com>
2020-04-10 03:20:02 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,21 +1,27 @@
 class Triangle
-  def initialize(side_1,side_2,side_3)
-    @side_1 = side_1
-    @side_2 = side_2
-    @side_3 = side_3
-  end
-
-  def kind()
-    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
-      raise TriangleError
-    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
-      raise TriangleError
-    else
-      if (@side_1 == @side_2) && (@side_2 == @side_3)
+ attr_accessor :length1, :length2, :length3
+ 
+ def initialize(length1, length2, length3)
+   @length3 = length3
+   @length2 = length2
+   @length1 = length1
+   @all_lengths = []
+   @all_lengths << @length1 
+   @all_lengths << @length2
+   @all_lengths << @length3
+ end
+ 
+ def kind()
+   if @all_lengths.any?{|length| length == 0}
+     raise TriangleError
+   elsif (@length1+@length2 <= @length3) || (@length1+@length3 <= @length2) || (@length2+@length3 <= @length1)
+   raise TriangleError
+ else
+      if (@length1 == @length2) && (@length2 == @length3)
         :equilateral
-      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+      elsif (@length1 == @length2) || (@length2 == @length3) || (@length1 == @length3)
         :isosceles
-      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+      elsif (@length1 != @length2) && (@length2 != @length3) && (@length1 != @length3)
         :scalene
       end
     end
@@ -25,5 +31,5 @@ class Triangle
 end
 
 class TriangleError < StandardError
-  # triangle error code
+  
 end
